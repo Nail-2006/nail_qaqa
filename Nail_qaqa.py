2d16c20fea32b3649a8839bf86dcbafb7ae2c0c6
Qədimov Nail Samir <nail_gc29@itstep.edu.az>
2024-12-09 16:44:44 +0400

    Initial Commit

---
+++ Nail_qaqa.py
@@ -0,0 +1,18 @@
+# поиск по имени
+
+games =[
+      ["pubg", 20],
+      ["cs-go", 0]
+]
+i=0
+a=0
+print("searching by variants\n"
+      "1-by name\n"
+      "2-by cost")
+user_choise = int(input("your choise:  "))
+while a<len(games):
+
+
+
+
+
